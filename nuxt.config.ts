e23d8af6f5e92353dc85f8ed916d2f4b15eb17b7
florentinduyckpro-commits <florentin.duyck.pro@gmail.com>
2026-08-31 16:56:43 +0200

    Add ESLint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/i18n'],
+  modules: ['@nuxt/content', '@nuxtjs/i18n', '@nuxt/eslint'],
   vite: {
     plugins: [tailwindcss()]
   },
